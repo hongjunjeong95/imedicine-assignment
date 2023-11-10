e8250426609a07c0b396f7570cb1124f6f337ab2
hongjunjeong <hongjunjeong95@gmail.com>
2023-11-10 16:26:05 +0900

    Initial

---
+++ src/test/kotlin/com/imedicine/imedicine/IMedicineApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.imedicine.imedicine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IMedicineApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
